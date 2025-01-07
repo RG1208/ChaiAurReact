c8b9932f0c97f956a9e3dd420313610f6c4473ce
RG1208 <rachitgarg1208@gmail.com>
2025-01-08 00:07:05 +0530

    BG CHANGER

--- 04BgChanger/src/App.jsx
+++ 04BgChanger/src/App.jsx
@@ -7,8 +7,9 @@ function App() {
       <div className='flex flex-wrap fixed bottom-12 justify-center inset-x-0 px-2'>
         <div className='flex flex-wrap justify-center bg-white px-3 py-3 rounded-3xl gap-3 shadow-xl'>
 
-          <button onClick={() => { setColor("red") }} className='outline rounded-full px-2 py-1 text-white 'style={{backgroundColor:"Red"}}>Red</button>
-          <button onClick={() => { setColor("yellow") }} className='outline rounded-full px-2 py-1 text-black 'style={{backgroundColor:"yellow"}}>yellow</button>
+          <button onClick={() => { setColor("red") }} className='outline roundged-full px-2 py-1 text-white 'style={{backgroundColor:"Red"}}>Red</button>
+          <button onClick={() => { setColor("yellow") }} className='outline rounded-full
+           px-2 py-1 text-black 'style={{backgroundColor:"yellow"}}>yellow</button>
           <button onClick={() => { setColor("blue") }} className='outline rounded-full px-2 py-1 text-white 'style={{backgroundColor:"blue"}}>blue</button>
           <button onClick={() => { setColor("orange") }} className='outline rounded-full px-2 py-1 text-white 'style={{backgroundColor:"orange"}}>orange</button>
           <button onClick={() => { setColor("black") }} className='outline rounded-full px-2 py-1 text-white 'style={{backgroundColor:"black"}}>black</button>
